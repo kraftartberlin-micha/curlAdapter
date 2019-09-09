be449332d9de6f30ef7c32913363fe479c361b60
Michael Kraft <micha@kraft-art-berlin.de>
2019-09-09 13:00:19 +0200

    first draw without test

---
+++ CurlAdapter.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+class CurlAdapter
+{
+    /**
+     * @var string
+     */
+    private $responseCode;
+
+    /**
+     * @var bool|string
+     */
+    private $responseBody;
+
+    /**
+     * @var array
+     */
+    private $options;
+
+    public function __construct(array $options = [])
+    {
+        $this->curlHandler = curl_init();
+        $this->options = $options;
+    }
+
+    private function setOpt($optionKey, $optionValue)
+    {
+        curl_setopt($this->curlHandler, $optionKey, $optionValue);
+    }
+
+    public function fireRequest($url, $method = 'get', $data = [])
+    {
+        if (strtolower($method) === 'post') {
+            curl_setopt($this->curlHandler, CURLOPT_POST, 1);
+            curl_setopt($this->curlHandler, CURLOPT_POSTFIELDS, $data);
+        } else {
+            $url .= '?' . http_build_query($data);
+        }
+
+        $this->setOpt(CURLOPT_URL, $url);
+        //$this->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+        $this->setOpt(CURLOPT_RETURNTRANSFER, true);
+
+        foreach($this->options as $key => $value){
+            $this->setOpt($key, $value);
+        }
+
+        $responseBody = curl_exec($this->curlHandler);
+        if(is_string($responseBody)){
+            $this->setResponseBody($responseBody);
+        }
+
+        $this->setResponseCode(curl_getinfo($this->curlHandler, CURLINFO_RESPONSE_CODE));
+
+        if (is_resource($this->curlHandler)) {
+            curl_close($this->curlHandler);
+        }
+        return curl_error($this->curlHandler) ?: $this->responseBody;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResponseCode()
+    {
+        return $this->responseCode;
+    }
+
+    /**
+     * @param string $responseCode
+     */
+    private function setResponseCode($responseCode): void
+    {
+        $this->responseCode = $responseCode;
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function getResponseBody()
+    {
+        return $this->responseBody;
+    }
+
+    /**
+     * @param bool|string $responseBody
+     */
+    private function setResponseBody($responseBody): void
+    {
+        $this->responseBody = $responseBody;
+    }
+}
